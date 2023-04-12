5ce4ad99bcc1ae352723649d5a1c525f6b54cbec
nguch <kelvin.1.ngure@gmail.com>
2023-04-12 23:45:27 +0300

    set up amphibians repository. interface and class

--- app/src/main/java/com/example/amphibians/data/AmphibianRepository.kt
+++ app/src/main/java/com/example/amphibians/data/AmphibianRepository.kt
@@ -1,5 +1,13 @@
 package com.example.amphibians.data
 
-interface AmphibianRepository {
+import com.example.amphibians.models.Amphibian
+import com.example.amphibians.network.AmphibiansInfoService
 
+interface AmphibianRepository {
+    suspend fun getAmphibians() : List<Amphibian>
+}
+
+class AmphibianNetworkRepository (private val retrofitService : AmphibiansInfoService) : AmphibianRepository{
+    override suspend fun getAmphibians() : List<Amphibian> = retrofitService.getAmphibians()
+    // we're equating the first function to the second. so when the first is called, it calls the second. i.e the first returns what the second returns
 }
