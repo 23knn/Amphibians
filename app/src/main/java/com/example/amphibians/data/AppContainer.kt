9b7a6f5ca124c60c5f01d8bafff3e2ac1eb14f44
nguch <kelvin.1.ngure@gmail.com>
2023-04-12 23:54:10 +0300

    created the app container and added retrofit and the repository to it

--- app/src/main/java/com/example/amphibians/data/AppContainer.kt
+++ app/src/main/java/com/example/amphibians/data/AppContainer.kt
@@ -1,9 +1,24 @@
-package com.example.amphibians.container
+package com.example.amphibians.data
 
-interface AppContainer {
+import com.example.amphibians.network.AmphibiansInfoService
+import retrofit2.Retrofit
 
+interface AppContainer {// returns the repository after building and all
+    val amphibianRepository : AmphibianRepository
 }
 
 class DefaultAppContainer : AppContainer{
+    val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com/amphibians"
 
+    // build retrofit
+    val retrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .build()
+
+    // use retrofit to make a service
+    val retrofitService : AmphibiansInfoService by lazy { retrofit.create( AmphibiansInfoService::class.java ) }
+    // the service is created lazily. i.e when needed. just pass the service class lambda so retrofit can use it later
+
+    // provide the service to the repository
+    override val amphibianRepository: AmphibianRepository by lazy { AmphibianNetworkRepository(retrofitService) }
 }
